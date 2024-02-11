ae0cfd30227e0903f18f6228c421f3e6680c56bb
zerksis umrigar <umrigar@binghamton.edu>
2024-02-10 21:41:04 -0500

    stat/reporting updates

--- dist/lib/run-test-suites.js
+++ dist/lib/run-test-suites.js
@@ -1,28 +1,38 @@
 import { Errors } from 'cs544-js-utils';
-//cwd == /autograder/submission
 export default async function runTestSuites(descr, suites) {
     const testCaseInfos = [];
     let output = `# ${descr}`;
+    const results = {};
+    let [nTotalAcc, nFailedAcc] = [0, 0];
+    let abortSuite = '';
     for (const suite of suites) {
         let suiteName = 'UNKNOWN';
         try {
             suiteName = suite.opts.name;
-            output += `## ${suiteName}`;
             const suiteResult = await suite.run();
             if (!suiteResult.isOk)
                 return suiteResult;
             const suiteInfos = suiteResult.val;
-            testCaseInfos.push(...suiteResult.val);
-            const doAbort = suite.opts.abortOnFail
-                && suiteInfos.some(info => info.status === 'failed');
+            testCaseInfos.push(...suiteInfos);
+            const failInfos = suiteInfos.filter(info => info.status === 'failed');
+            const [nTotal, nFailed] = [suiteInfos.length, failInfos.length];
+            nTotalAcc += nTotal;
+            nFailedAcc += nFailed;
+            output += `**${suiteName}**: ${nFailed}/${nTotal} failures \n`;
+            results[suiteName] = { nTotal, nFailed };
+            const doAbort = suite.opts.abortOnFail && nFailed > 0;
             if (doAbort) {
-                output += `tests aborted on failing test suite ${suiteName}`;
+                abortSuite = suiteName;
                 break;
             }
         }
         catch (err) {
             return Errors.errResult(`error running testsuite ${suiteName}: ${err}`);
         }
+    }
+    output += `**Total**: ${nFailedAcc}/${nTotalAcc} failures \n`;
+    if (abortSuite) {
+        output += `tests aborted on failing test suite ${abortSuite}`;
     }
     const gradescopeResults = makeGradescopeResults(output, testCaseInfos);
     return Errors.okResult(gradescopeResults);
--- src/lib/run-test-suites.ts
+++ src/lib/run-test-suites.ts
@@ -10,31 +10,42 @@ import { TestSuite, TestCase, TestCaseInfo, GradescopeResults,
 
 //cwd == /autograder/submission
 
+type SuiteCounts = { nTotal: number, nFailed: number };
 export default async function
 runTestSuites(descr: string, suites: TestSuite[])
   : Promise<Errors.Result<GradescopeResults>>
 {
   const testCaseInfos : TestCaseInfo[] = [];
   let output =  `# ${descr}`;
+  const results: Record<string, SuiteCounts> = {};
+  let [ nTotalAcc, nFailedAcc ] = [ 0, 0 ];
+  let abortSuite = '';
   for (const suite of suites) {
     let suiteName = 'UNKNOWN';
     try {
       suiteName = suite.opts.name;
-      output += `## ${suiteName}`;
       const suiteResult = await suite.run();
       if (!suiteResult.isOk) return suiteResult;
       const suiteInfos = suiteResult.val;
-      testCaseInfos.push(...suiteResult.val);
-      const doAbort = suite.opts.abortOnFail
-	&& suiteInfos.some(info => info.status === 'failed');
+      testCaseInfos.push(...suiteInfos);
+      const failInfos = suiteInfos.filter(info => info.status === 'failed');
+      const [ nTotal, nFailed ] = [ suiteInfos.length, failInfos.length ];
+      nTotalAcc += nTotal; nFailedAcc += nFailed;
+      output += `**${suiteName}**: ${nFailed}/${nTotal} failures \n`;
+      results[suiteName] = { nTotal, nFailed };
+      const doAbort = suite.opts.abortOnFail && nFailed > 0;
       if (doAbort) {
-	output += `tests aborted on failing test suite ${suiteName}`;
+	abortSuite = suiteName;
 	break;
       }
     }
     catch (err) {
       return Errors.errResult(`error running testsuite ${suiteName}: ${err}`);
     }
+  }
+  output += `**Total**: ${nFailedAcc}/${nTotalAcc} failures \n`;
+  if (abortSuite) {
+    output += `tests aborted on failing test suite ${abortSuite}`;
   }
   const gradescopeResults = makeGradescopeResults(output, testCaseInfos);
   return Errors.okResult(gradescopeResults);
