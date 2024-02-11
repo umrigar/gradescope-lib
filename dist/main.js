4436c100a0da721cdf7a1b145bf8a08ef4db5c11
zerksis umrigar <umrigar@binghamton.edu>
2024-02-11 16:11:03 -0500

    export mocha tests

--- dist/main.js
+++ dist/main.js
@@ -2,5 +2,6 @@ export * as GradeTypes from './lib/base.js';
 import runTestSuites from './lib/run-test-suites.js';
 import makePathTest from './lib/path-test.js';
 import makeCmdTest from './lib/cmd-test.js';
-export { runTestSuites, makePathTest, makeCmdTest, };
+import makeMochaSuite from './lib/mocha-suite.js';
+export { runTestSuites, makePathTest, makeCmdTest, makeMochaSuite, };
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ export * as GradeTypes from './lib/base.js';
 import runTestSuites from './lib/run-test-suites.js';
 import makePathTest from './lib/path-test.js';
 import makeCmdTest from './lib/cmd-test.js';
+import makeMochaSuite from './lib/mocha-suite.js';
 
 export {
   runTestSuites,
   makePathTest,
   makeCmdTest,
+  makeMochaSuite,
 };
